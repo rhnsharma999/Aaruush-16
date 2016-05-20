ecd695e8d288c8a9dae83b297124b81945f031f6
rhnsharma999 <rhnsharma999@gmail.com>
2016-05-21 01:09:47 +0530

    Sign in UI

---
+++ Aaruush/SecondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.swift
+//  Aarush
+//
+//  Created by Rohan Lokesh Sharma on 19/05/16.
+//  Copyright © 2016 rohan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func goBack(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Aaruush/ViewController.swift
+++ Aaruush/ViewController.swift
@@ -1,25 +1,131 @@
 //
 //  ViewController.swift
-//  Aaruush
+//  Aarush
 //
-//  Created by Rohan Lokesh Sharma on 21/05/16.
+//  Created by Rohan Lokesh Sharma on 16/05/16.
 //  Copyright © 2016 rohan. All rights reserved.
 //
 
 import UIKit
+import StarWars
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIViewControllerTransitioningDelegate{
+    
+    var a = 1;
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var firstLogo: UILabel!
+     @IBOutlet var secondLogo: UILabel!
+     @IBOutlet var signInButton: UIButton!
     override func viewDidLoad() {
+        
+        signInButton.layer.borderWidth = 2;
+        signInButton.layer.borderColor = UIColor.yellowColor().CGColor
+        signInButton.layer.cornerRadius = 10;
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+   
+    override func viewWillAppear(animated: Bool)
+    {
+        
+    
+        imageView.alpha = 0.0;
+        firstLogo.alpha = 0.0;
+        secondLogo.alpha = 0.0;
+        signInButton.alpha = 0.0;
+        
+        
+        
+       
+        
+    }
+   
+    override func viewDidAppear(animated: Bool)
+    {
+        
+        
+       
+        
+        UIView.animateWithDuration(0.5, delay: 0.5, options: [.CurveEaseOut], animations: {
+            
+           self.imageView.alpha = 1.0
+            
+            }, completion:
+            
+            { (value: Bool) in
+                
+                
+                UIView.animateWithDuration(0.5, delay: 0.0, options: [.CurveEaseOut], animations: {
+                    
+                    self.firstLogo.alpha = 1.0
+                    
+                    }, completion:
+                    
+                    { (value: Bool) in
+                        
+                        UIView.animateWithDuration(0.5, delay: 0.0, options: [.CurveEaseOut], animations: {
+                            
+                            self.secondLogo.alpha = 1.0
+                            
+                            }, completion:
+                            
+                            { (value: Bool) in
+                                
+                                UIView.animateWithDuration(0.5, delay: 0.0, options: [.CurveEaseOut], animations:
+                                    {
+                                    
+                                    self.signInButton.alpha = 1.0
+                                     
+                                    
+                                }, completion: nil)
+                     })
+                 })
+            })
+ 
+    }
+   
+  
+    @IBAction func signIn(sender: AnyObject) {
+        
+        UIView.animateWithDuration(0.1, delay: 0.0, options: [], animations: {self.signInButton.backgroundColor = UIColor.yellowColor()}, completion: nil)
+        
+        UIView.animateWithDuration(0.1, delay: 0.1, options: [], animations: {self.signInButton.backgroundColor = UIColor.clearColor()}, completion: {(value :Bool) in
+            
+            self.performSegueWithIdentifier("login", sender: self)
+        
+        
+        })
+        
+        
+    }
+    
+    
+   
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+        let vc = segue.destinationViewController;
+        vc.transitioningDelegate = self;
+        
+        
+        
+    }
+    
+     func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return StarWarsGLAnimator()
+    }
 
+   
 
+    //func animationControllerForPresentedController(presented: UIViewController, presentingController presenting:
 }
 
