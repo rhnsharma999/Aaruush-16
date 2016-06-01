770d399dbed85d9b9c099aaf77d57c46f74b5224
rhnsharma999 <rhnsharma999@gmail.com>
2016-06-01 22:01:20 +0530

    Constraints Added

--- Aaruush/SecondViewController.swift
+++ Aaruush/SecondViewController.swift
@@ -11,11 +11,16 @@ import UIKit
 class SecondViewController: UIViewController {
     
     @IBOutlet var balloon1:UIImageView!
-    @IBOutlet var balloon2:UIImageView!
+//    @IBOutlet var balloon2:UIImageView!
     @IBOutlet var balloon3:UIImageView!
     
     @IBOutlet var smallBalloon1:UIImageView!
     @IBOutlet var smallBalloon2:UIImageView!
+    
+    @IBOutlet var cloud1:UIImageView!;
+    @IBOutlet var cloud2:UIImageView!;
+    @IBOutlet var cloud3:UIImageView!;
+    @IBOutlet var cloud4:UIImageView!;
     
 
     @IBOutlet var sunImage: UIImageView!
@@ -26,17 +31,34 @@ class SecondViewController: UIViewController {
     }
     
     override func viewWillAppear(animated: Bool) {
+  
+        
         balloon1.alpha = 0.0
-        balloon2.alpha = 0.0
+      
         balloon3.alpha = 0.0
     }
     
     override func viewDidAppear(animated: Bool) {
+      
+        
+        
+        //--------------animations-------------------->
+        
+    
+        UIView.animateWithDuration(20, delay: 0.5, options: [.Repeat,.Autoreverse], animations: {
+            
+            self.cloud1.center.x += 30
+            self.cloud2.center.x += 30
+            self.cloud3.center.x += 30
+            self.cloud4.center.x += 30
+            
+            
+            }, completion: nil);
         
         
         
         
-        UIView.animateWithDuration(5, delay: 0.0, options: [.Repeat,.CurveLinear], animations: {
+        UIView.animateWithDuration(15, delay: 0.0, options: [.Repeat,.CurveLinear], animations: {
             
             self.sunImage.transform = CGAffineTransformMakeRotation((360.0 * CGFloat(M_PI)) / 360.0)
             
@@ -62,19 +84,22 @@ class SecondViewController: UIViewController {
             {
             
             self.balloon1.alpha = 1.0
-            self.balloon2.alpha = 1.0
+          //  self.balloon2.alpha = 1.0
             self.balloon3.alpha = 1.0
 
             }, completion: { (value: Bool) in
 
                 UIView.animateWithDuration(3, delay: 0.0, options: [.Autoreverse,.Repeat], animations: {
                     
-                    self.balloon1.center.x += 20;
-                    self.balloon2.center.x += 20;
-                    self.balloon3.center.x += 20;
+                    self.balloon1.center.y += 20;
+            //        self.balloon2.center.y += 20;
+                    self.balloon3.center.y += 20;
                     
                     }, completion: nil)
         })
+        
+        
+        //--------------end animations-------------------->
     }
 
     override func didReceiveMemoryWarning() {
